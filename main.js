de9d62fbace8b985bd85459a722c5d5fe1da9658
Carlos-Ortiz-Mestre-6602 <comestre66@gmail.com>
2025-08-27 11:02:48 -0400

    Agregando base de datos

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const sqlite3 = require('sqlite3').verbose()
 const path = require('node:path')
+
+let db;
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -14,6 +17,18 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
+
+  const dbPath = path.join(__dirname, 'database.db');
+  
+  db = new sqlite3.Database(dbPath, (err) => {
+    if (err) console.error('Error opening database', err);
+    else console.log('Database connected');
+  });
+
+  // Handlers
+  getUsersHandler(db);
+
+  // Create window
   createWindow()
 
   app.on('activate', () => {
@@ -23,8 +38,27 @@ app.whenReady().then(() => {
   })
 })
 
+// Handlers
+function getUsersHandler(db) {
+
+  ipcMain.handle('get-users', async () => {
+    return new Promise((resolve, reject) => {
+      db.all('SELECT id, name FROM users', (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  });
+
+}
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
+console.log("From server...")
